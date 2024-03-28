73c082867337ad931bfa71c0fe19a271df6d1eda
Marco Fura <mfura@kataricorp.com>
2024-03-28 00:08:06 -0300

    Initial Commit

---
+++ Play Sound/Play_SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_SoundApp.swift
+//  Play Sound
+//
+//  Created by Marco Fura on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Play_SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
